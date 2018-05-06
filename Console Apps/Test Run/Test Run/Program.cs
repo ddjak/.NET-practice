a6ddaa5cde69e52e66fbd57a5bcb6ddb512a260c
admin <drdjakovic@gmail.com>
2018-05-06 00:06:28 -0400

    OOP

---
+++ Console Apps/Test Run/Test Run/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Run
+{
+    class Program
+    {
+        class Person
+        {
+            int age;
+            string name;
+            public void sayHi()
+            {
+                Console.WriteLine("Hey there");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+        int sum(int num1, int num2) {
+            return num1 + num2;
+        }
+            Console.WriteLine(sum(2, 3));
+
+            Person frank = new Person();
+            frank.sayHi();
+
+            Console.ReadLine();
+        }
+    }
+}
